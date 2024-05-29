b6e7b71b8e04fa2448a0d02996572af6df49e999
chai <chainot@hotmail.co.th>
2024-05-29 15:39:53 +0700

    clean up and check item merge

--- src/main/java/sypztep/arctechz/mixin/featureMerge/ItemEntityMixin.java
+++ src/main/java/sypztep/arctechz/mixin/featureMerge/ItemEntityMixin.java
@@ -4,8 +4,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,21 +13,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import sypztep.arctechz.ModConfig;
 import sypztep.arctechz.common.util.ArctechzUtil;
+import sypztep.arctechz.mixin.featureMerge.util.ItemEntityAccessor;
 
-import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
-    @Shadow
-    public abstract ItemStack getStack();
-
-    @Shadow public abstract int getItemAge();
 
     @Shadow private int pickupDelay;
-
-    @Shadow private int itemAge;
+    @Unique
+    private static final float RANGE = 1.5f;
 
     public ItemEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -38,32 +31,30 @@ public abstract class ItemEntityMixin extends Entity {
     @Inject(method = "tick", at = @At("HEAD"))
     public void onTick(CallbackInfo ci) {
         if (!this.getWorld().isClient) {
-            List<ItemEntity> list = this.getWorld().getEntitiesByClass(ItemEntity.class, this.getBoundingBox().expand(1.5, 0.5, 1.5), (otherItemEntity) -> true);
-            ItemStack stack = this.getStack();
-            String itemName = stack.getName().getString();
+            List<ItemEntity> nearbyItems = this.getWorld().getEntitiesByClass(
+                    ItemEntity.class,
+                    this.getBoundingBox().expand(RANGE),
+                    item -> item != (Object) this && canMerge((ItemEntity) (Object) this, item)
+            );
 
-            this.setCustomName(Text.literal((stack.getCount() + " ")).formatted(Formatting.WHITE)
-                    .append(Text.literal("x ").formatted(Formatting.WHITE))
-                    .append(Text.literal(itemName).formatted(Formatting.GOLD)).formatted(Formatting.BOLD));
-            updateCustomName((ItemEntity)(Object) this);
-            for (int i = 0; i < list.size(); i++) {
-                ItemEntity item1 = list.get(i);
-                for (int j = i + 1; j < list.size(); j++) {
-                    ItemEntity item2 = list.get(j);
-                    if (item1 != item2 && canMerge(item1, item2)) {
-                        mergeItems(item1, item2);
-                    }
+            for (ItemEntity otherItem : nearbyItems) {
+                if (canMerge((ItemEntity) (Object) this, otherItem)) {
+                    mergeItems((ItemEntity) (Object) this, otherItem);
+                    break; // Exit after merging to prevent excessive merges in one tick
                 }
             }
+
+            updateCustomName((ItemEntity) (Object) this);
         }
     }
+
     @Unique
     private boolean canMerge(ItemEntity entity1, ItemEntity entity2) {
         ItemStack stack1 = entity1.getStack();
         ItemStack stack2 = entity2.getStack();
         return entity1.isAlive() && entity2.isAlive()
                 && ItemStack.areItemsAndComponentsEqual(stack1, stack2)
-                && this.pickupDelay != 32767 && this.itemAge != -32768 && this.itemAge < 6000;
+                && this.pickupDelay != 32767 && entity2.getItemAge() != -32768 && entity2.getItemAge() < 6000;
     }
 
     @Unique
@@ -72,20 +63,44 @@ public abstract class ItemEntityMixin extends Entity {
         ItemStack stack2 = entity2.getStack();
         int totalAmount = stack1.getCount() + stack2.getCount();
 
-        if (totalAmount <= ModConfig.stackSize) {
-            stack2.setCount(totalAmount);
-            entity1.discard();
+        // Try merging a few times
+        boolean merged = false;
+        for (int i = 0; i < 3; i++) {  // Number of retry attempts
+            ((ItemEntityAccessor) entity1).arctechz$tryMerge(entity2);
+            if (!entity1.isAlive()) {  // Check if entity1 was merged and discarded
+                merged = true;
+                break;
+            }
+        }
+
+        // If not merged after retries, set the count directly
+        if (!merged) {
+            if (totalAmount <= ModConfig.stackSize) {
+                stack2.setCount(totalAmount);
+                entity1.discard();
+            } else {
+                int remainder = totalAmount - ModConfig.stackSize;
+                stack2.setCount(ModConfig.stackSize);
+                stack1.setCount(remainder);
+                updateCustomName(entity1); // Update the custom name of the remaining entity
+            }
         }
         updateCustomName(entity2);
     }
+    @Inject(method = "tryMerge()V" , at = @At("HEAD"), cancellable = true)
+    private void disableVanillaMerge(CallbackInfo ci) {
+        ci.cancel();
+    }
+
     @Unique
-    private void updateCustomName(ItemEntity entity){
+    private void updateCustomName(ItemEntity entity) {
         ItemStack stack = entity.getStack();
         String itemName = stack.getName().getString();
-        int i = stack.getCount();
-        entity.setCustomName(ArctechzUtil.createCustomName(i,itemName));
+        int count = stack.getCount();
+        entity.setCustomName(ArctechzUtil.createCustomName(count, itemName));
     }
 }
+
 
 
 
