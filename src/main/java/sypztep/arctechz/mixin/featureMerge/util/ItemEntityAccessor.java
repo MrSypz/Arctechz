6263e5372a1aab023e51b7c276cc5c846234aa30
chai <chainot@hotmail.co.th>
2024-05-29 15:49:08 +0700

    clean up and check item merge

---
+++ src/main/java/sypztep/arctechz/mixin/featureMerge/util/ItemEntityAccessor.java
@@ -0,0 +1,11 @@
+package sypztep.arctechz.mixin.featureMerge.util;
+
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ItemEntity.class)
+public interface ItemEntityAccessor {
+	@Invoker("tryMerge")
+	void arctechz$tryMerge(ItemEntity other);
+}
