9d5fbc3b8344d5398b89c8fe4f582d762b708945
chai <chainot@hotmail.co.th>
2024-06-03 18:03:14 +0700

    Add Datagen not emblem and add translate to keybind

--- src/main/java/sypztep/arctechz/data/ModLanguageGenerator.java
+++ src/main/java/sypztep/arctechz/data/ModLanguageGenerator.java
@@ -2,7 +2,6 @@ package sypztep.arctechz.data;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.registry.RegistryWrapper;
 import sypztep.arctechz.Arctechz;
 
@@ -16,8 +15,8 @@ public class ModLanguageGenerator extends FabricLanguageProvider  {
     @Override
     public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder translationBuilder) {
         addcallbackslot(translationBuilder,"Fail to drop backslot item switch to main hand first!");
-        addconfig(translationBuilder,"title","Arctechz Main");
 
+        addconfig(translationBuilder,"title","Arctechz Main");
         addconfig(translationBuilder,"featureMerge","Merge Feature");
         addconfig(translationBuilder,"stackSize","Max Merge Size");
         addconfig(translationBuilder,"mergeRange","Max Merge Range");
@@ -25,14 +24,29 @@ public class ModLanguageGenerator extends FabricLanguageProvider  {
         addconfig(translationBuilder,"enum.CosmeticsOptions.ENABLE","Enable");
         addconfig(translationBuilder,"enum.CosmeticsOptions.FIRST_PERSON","First Person");
         addconfig(translationBuilder,"enum.CosmeticsOptions.DISABLE","Disable");
-
         addconfig(translationBuilder,"category.client","Arctechz Client Side");
         addconfig(translationBuilder,"viewItemDistance","viewItemDistance");
+        addkeycategory(translationBuilder);
+        addkeybind(translationBuilder,"select_weapon","Select Weapon");
+        addkeybind(translationBuilder,"select_weapon","Select Weapon");
+        addkeybind(translationBuilder,"swap_weapon","Swap Weapon");
+
+        addentity(translationBuilder,"raven","Raven");
     }
     private static void addcallbackslot(TranslationBuilder translationBuilder,String message) {
         translationBuilder.add(Arctechz.id("backslot.feature.dropfail").toString(),message);
     }
+
     private static void addconfig(TranslationBuilder translationBuilder,String configname,String message) {
         translationBuilder.add(Arctechz.MODID + ".midnightconfig" + "." + configname,message);
+    }
+    private static void addkeybind(TranslationBuilder translationBuilder,String configname,String message) {
+        translationBuilder.add("key."+ Arctechz.MODID + "." + configname,message);
+    }
+    private static void addkeycategory(TranslationBuilder translationBuilder) {
+        translationBuilder.add("key.categories" + Arctechz.MODID , "Arctechz");
+    }
+    private static void addentity(TranslationBuilder translationBuilder,String configname,String message) {
+        translationBuilder.add("entity."+ Arctechz.MODID + "." + configname,message);
     }
 }
