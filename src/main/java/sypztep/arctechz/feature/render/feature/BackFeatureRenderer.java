2c80749aebe0ef6b54bca868ed6205cde34e8c91
chai <chainot@hotmail.co.th>
2024-09-11 17:41:06 +0700

    Simpify?

--- src/main/java/sypztep/arctechz/feature/render/feature/BackFeatureRenderer.java
+++ src/main/java/sypztep/arctechz/feature/render/feature/BackFeatureRenderer.java
@@ -23,62 +23,66 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BackFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
-	private final Map<String, ResolveBackData> models;
+    private final Map<String, ResolveBackData> models;
 
-	public BackFeatureRenderer(FeatureRendererContext<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> featureRendererContext, EntityRendererFactory.Context loader) {
-		super(featureRendererContext);
-		this.models = ArctechzFeature.BACKCOSMETIC_DATA.entrySet().stream()
-			.collect(Collectors.toMap(Map.Entry::getKey, data -> new ResolveBackData(data.getValue().getTexture(), data.getValue().createModel(loader))));
-	}
+    public BackFeatureRenderer(FeatureRendererContext<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> featureRendererContext, EntityRendererFactory.Context loader) {
+        super(featureRendererContext);
+        this.models = ArctechzFeature.BACKCOSMETIC_DATA.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, data -> new ResolveBackData(data.getValue().getTexture(), data.getValue().createModel(loader))));
+    }
 
-	@Override
-	public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
 
-		PlayerCosmeticData cosmeticData = ArctechzFeature.getCosmeticData(entity);
-		if (ModConfig.shouldDisplayCosmetics() && cosmeticData != null && !entity.isInvisible()) {
-			String playerBackData = cosmeticData.getBack();
-			if (playerBackData != null) {
-				ResolveBackData resolveBackData = this.models.get(playerBackData);
-				if (resolveBackData != null) {
+        PlayerCosmeticData cosmeticData = ArctechzFeature.getCosmeticData(entity);
+        if (ModConfig.shouldDisplayCosmetics() && cosmeticData != null && !entity.isInvisible()) {
+            String playerBackData = cosmeticData.getBack();
+            if (playerBackData != null) {
+                ResolveBackData resolveBackData = this.models.get(playerBackData);
+                if (resolveBackData != null) {
 
-					Identifier texture = resolveBackData.texture();
-					BackCosmeticModel model = resolveBackData.model();
+                    Identifier texture = resolveBackData.texture();
+                    BackCosmeticModel model = resolveBackData.model();
 
-					if (model instanceof WiggleFeature) {
-						float previousWiggleAmount = 0.0f;
-						long gameTime = entity.getWorld().getTime();
-						float wiggleIntensity = 1.5f + Math.min(entity.getHealth() * 0.1f, 2);
-						float wiggleSpeed = 0.1f;
-						float targetWiggleAmount = MathHelper.sin(gameTime * wiggleSpeed) * wiggleIntensity;
+                    if (model instanceof WiggleFeature) {
+                        float currentWiggleAmount = getCurrentWiggleAmount(entity);
+                        float q = 0;
+                        float t = MathHelper.lerp(tickDelta, entity.prevStrideDistance, entity.strideDistance);
+                        q += MathHelper.sin(MathHelper.lerp(tickDelta, entity.prevHorizontalSpeed, entity.horizontalSpeed) * 2.0f) * 3.0f * t;
 
-						float lerpSpeed = 0.1f;
-						float currentWiggleAmount = MathHelper.lerp(lerpSpeed, previousWiggleAmount, targetWiggleAmount);
-						float q = 0;
-						float t = MathHelper.lerp(tickDelta, entity.prevStrideDistance, entity.strideDistance);
-						q += MathHelper.sin(MathHelper.lerp(tickDelta, entity.prevHorizontalSpeed, entity.horizontalSpeed)) * 3.0f * t;
+                        matrices.push();
+                        model.back.pivotX = this.getContextModel().body.pivotX;
+                        model.back.pivotY = this.getContextModel().body.pivotY;
+                        model.back.pitch = this.getContextModel().body.pitch + Math.abs(q);
+                        model.back.yaw = this.getContextModel().body.yaw + currentWiggleAmount;
 
-						matrices.push();
-						model.back.pivotX = this.getContextModel().body.pivotX;
-						model.back.pivotY = this.getContextModel().body.pivotY;
-						model.back.pitch = this.getContextModel().body.pitch + Math.abs(q);
-						model.back.yaw = this.getContextModel().body.yaw + currentWiggleAmount;
+                        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(texture));
+                        model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
 
-						VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(texture));
-						model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
+                        matrices.pop();
+                    } else {
+                        model.back.pivotX = this.getContextModel().body.pivotX;
+                        model.back.pivotY = this.getContextModel().body.pivotY;
+                        model.back.pitch = this.getContextModel().body.pitch;
+                        model.back.yaw = this.getContextModel().body.yaw;
+                        model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(texture)), light, OverlayTexture.DEFAULT_UV);
+                    }
+                }
+            }
+        }
+    }
 
-						matrices.pop();
-					} else {
-						model.back.pivotX = this.getContextModel().body.pivotX;
-						model.back.pivotY = this.getContextModel().body.pivotY;
-						model.back.pitch = this.getContextModel().body.pitch;
-						model.back.yaw = this.getContextModel().body.yaw;
-						model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(texture)), light, OverlayTexture.DEFAULT_UV);
-					}
-				}
-			}
-		}
-	}
+    private static float getCurrentWiggleAmount(AbstractClientPlayerEntity entity) {
+        float previousWiggleAmount = 0.0f;
+        long gameTime = entity.getWorld().getTime();
+        float wiggleIntensity = 1.5f + Math.min(entity.getHealth() * 0.1f, 2);
+        float wiggleSpeed = 0.1f;
+        float targetWiggleAmount = MathHelper.sin(gameTime * wiggleSpeed) * wiggleIntensity;
 
-	private record ResolveBackData(Identifier texture, BackCosmeticModel model) {
-	}
+        float lerpSpeed = 0.1f;
+        return MathHelper.lerp(lerpSpeed, previousWiggleAmount, targetWiggleAmount);
+    }
+
+    private record ResolveBackData(Identifier texture, BackCosmeticModel model) {
+    }
 }
